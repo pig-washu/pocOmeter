5645d2357622a5066240d6a4fcfd94b0cb95d21e
pig-washu <cmetts@path.wustl.edu>
2014-12-16 14:43:07 -0600

    add excel document

--- pocOmeter/MainWindow.xaml.vb
+++ pocOmeter/MainWindow.xaml.vb
@@ -1,3 +1,30 @@
-﻿Class MainWindow 
+﻿Imports System.Data
+
+
+Class MainWindow
+
+
+    Dim cnn As New ADODB.Connection
+    Dim rst As New ADODB.Recordset
+
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+        cnn.Open("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=test_nopid.xls;Extended Properties=Excel 8.0")
+
+        rst.Open("SELECT * FROM [Sheet1$];", cnn, 1, 1)
+
+        rst.MoveFirst()
+        Do While Not rst.EOF
+
+            Debug.Print(rst("MRN").Value)
+            rst.MoveNext()
+
+        Loop
+
+        cnn.Close()
+
+    End Sub
+
 
 End Class
